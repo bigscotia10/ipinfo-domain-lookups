481fa7854944dce7dffea503f116f480a7a82b88
bigscotia10 <chris.ferrell@pettygrovefull.com>
2023-03-22 09:11:36 -0700

    clean up

--- dnsLookup.js
+++ dnsLookup.js
@@ -1,18 +1,11 @@
-// Use the dnsLookup.js file locally in your terminal to get the ipinfo for the domain you list in the domains varialbe
-// make sure you have npm installed
-// then install the following:
-//// npm install axios
-//// npm install dns
-// Sign up for a free API from ipinfo here: https://ipinfo.io/
-// Replace TOKEN in the code with your ipinfo token
-// Run the script in your terminal doing the following: node dnsLookup.js
-// The script will return all the ipinfo org information for the associated domains.
-
 const dns = require('dns');
 const axios = require('axios');
 
 // Remove the HTTP and HTTPS. The domain should say something like www.domain.com or domain.com
 const domains = [
+  'www.example-domain.com',
+  'www.example-domain.com',
+  'www.example-domain.com',
   'www.example-domain.com',
 ];
 
